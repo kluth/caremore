9e2de20e3fdf071a6ef651d32d1be5b42915d81b
Matthias Kluth <kluthmatthias@gmail.com>
2021-02-08 07:40:02 +0100

    Commented out pug as it won't run on machine

--- app/app.js
+++ app/app.js
@@ -1,19 +1,20 @@
 import { App } from '@tinyhttp/app'
-import { pug } from '@tinyhttp/pug'
+//import { pug } from '@tinyhttp/pug'
 import { jwt } from '@tinyhttp/jwt'
 
-const app = pug()(new App({
+//const app = pug()(new App({
+const app = new App({
     noMatchHandler: (req, res) => res.send('Oops... something went wrong.')
 })
-.use(jwt({ secret: 'secret', algorithm: 'HS256'})))
+.use(jwt({ secret: 'secret', algorithm: 'HS256'}))
 
 app.get('/secret', function (req, res) {
     res.send(`Data inside payload: ${req['user']}`)
 })
 
 app.get('/', function (req, res) {
-    res.render('../app/views/index.pug')
-    //res.send('home')
+    //res.render('../app/views/index.pug')
+    res.send('home')
 })
 
 app.get('/login', function (req, res) {
