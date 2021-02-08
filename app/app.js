75530f9331c9a4763beb785e76a0a88a2b4415fe
Matthias Kluth <matthias@kluth.me>
2021-02-08 06:55:19 +0100

    Added pug

--- app/app.js
+++ app/app.js
@@ -1,17 +1,19 @@
 import { App } from '@tinyhttp/app'
+import { pug } from '@tinyhttp/pug'
 import { jwt } from '@tinyhttp/jwt'
 
-const app = new App({
+const app = pug()(new App({
     noMatchHandler: (req, res) => res.send('Oops... something went wrong.')
 })
-.use(jwt({ secret: 'secret', algorithm: 'HS256'}))
+.use(jwt({ secret: 'secret', algorithm: 'HS256'})))
 
 app.get('/secret', function (req, res) {
     res.send(`Data inside payload: ${req['user']}`)
 })
 
 app.get('/', function (req, res) {
-    res.send('home')
+    res.render('../app/views/index.pug')
+    //res.send('home')
 })
 
 app.get('/login', function (req, res) {
